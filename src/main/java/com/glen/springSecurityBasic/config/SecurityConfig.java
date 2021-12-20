10ef5601b67cccd3b3443b728bdae1e381a48efb
iss211 <glen.abraham@immcossi.com>
2021-12-20 12:12:08 +0530

    in memory user authentication with AuthenticationManagerBuilder

--- src/main/java/com/glen/springSecurityBasic/config/SecurityConfig.java
+++ src/main/java/com/glen/springSecurityBasic/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.glen.springSecurityBasic.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,34 +24,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	 */
 
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//		.antMatchers("/myAccount").authenticated()
-//		.antMatchers("/myBalance").authenticated()
-//		.antMatchers("/myLoans").authenticated()
-//		.antMatchers("/myCards").authenticated()
-//		.antMatchers("/notices").permitAll()
-//		.antMatchers("/contact").permitAll()
-//		.and()
-//		.formLogin()
-//		.and()
-//		.httpBasic();
-		
-		//Deny all requests
-//		http.authorizeRequests()
-//		.anyRequest()
-//		.denyAll()
-//		.and()
-//		.formLogin()
-//		.and()
-//		.httpBasic();
-		
-		//Permit All requests
 		http.authorizeRequests()
-		.anyRequest()
-		.permitAll()
+		.antMatchers("/myAccount").authenticated()
+		.antMatchers("/myBalance").authenticated()
+		.antMatchers("/myLoans").authenticated()
+		.antMatchers("/myCards").authenticated()
+		.antMatchers("/notices").permitAll()
+		.antMatchers("/contact").permitAll()
 		.and()
 		.formLogin()
 		.and()
 		.httpBasic();
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("password").authorities("admin")
+		.and().withUser("user").password("password").authorities("read")
+		.and().passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
 }
